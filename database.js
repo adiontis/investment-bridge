9ea6cda8cfc416c63d37fc48e3e4ca003d21fe17
adiontis <jevonmilam@gmail.com>
2025-08-18 21:08:59 -0500

    Update database.js

--- database.js
+++ database.js
@@ -2,8 +2,8 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const dbPath = process.env.NODE_ENV === 'production'
-? '/data/investment_bridge.db'
-: path.join(__dirname, 'investment_bridge.db');
+? '/data/microvest.db'
+: path.join(__dirname, 'microvest.db');
 
 const db = new sqlite3.Database(dbPath);
 
@@ -119,5 +119,3 @@ resolve();
 });
 });
 };
-
-module.exports = { db, initDatabase };
